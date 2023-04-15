63c335e025308866cb5cb59813a9b0b7e10e729b
XanderWatson <saahil_bhavsar@outlook.com>
2023-04-15 14:55:39 +0530

    Adds function to fetch date

--- utils/date.py
+++ utils/date.py
@@ -1,28 +1,24 @@
 from datetime import datetime
 
 import dateparser
-
-months = ['January', 'February', 'March', 'April', 'May', 'June',
-          'July', 'August', 'September', 'October', 'November', 'December']
-
-months_short = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
-                'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
-months.extend(months_short)
+from dateutil import parser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def is_date(text):
-    for month in months:
-        if month.lower() in text.lower():
-            return True
-    return False
+    try:
+        parser.parse(text)
+        return True
+    except ValueError:
+        return False
 
 
 def format_date(datestr):
     if datestr == "None":
         return ""
-
-    return f'{datetime.strptime(datestr, "%Y-%m-%d %H:%M:%S").day} {datetime.strftime(datetime.strptime(datestr, "%Y-%m-%d %H:%M:%S"), "%b")}'
+    
+    return f'{datetime.strptime(datestr, "%Y-%m-%d %H:%M:%S").day} {datetime.strftime(datetime.strptime(datestr, "%Y-%m-%d %H:%M:%S"), "%b %Y")}'
 
 
 def get_current_year():
@@ -38,3 +34,29 @@ def greater_than_today(datestr):
     date_today = datetime.now().date()
 
     return date >= date_today
+
+
+def get_date(url, script):
+    options = Options()
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-browser-side-navigation")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--headless")
+    options.add_argument(
+        'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+
+    driver = webdriver.Chrome(
+        options=options, executable_path="./chromedriver")
+
+    driver.get(url)
+    driver.implicitly_wait(5)
+
+    date = driver.execute_script(script)
+
+    driver.close()
+
+    return date
